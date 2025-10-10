eee95e984ea0105be28ef7df8d763c7a4aa19a4b
João Neto <joao72neto@gmail.com>
2025-10-10 12:52:30 -0300

    chore: create database tables

---
+++ backend/src/main/java/com/company/flowmodoro/model/entity/Sessions.java
@@ -0,0 +1,33 @@
+package com.company.flowmodoro.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Table(name = "sessions")
+@Builder
+@Data
+public class Sessions {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "ses_task")
+    private String task;    
+
+    @Column(name = "ses_focus")
+    private Double focus;
+
+    @Column(name = "ses_rest")
+    private Double rest;
+
+    @Column(name = "ses_interruptions")
+    private Integer interruptions;
+}
