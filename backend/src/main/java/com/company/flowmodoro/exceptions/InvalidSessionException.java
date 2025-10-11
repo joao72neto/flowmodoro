199ec8cd36e1b548b1f109c8c88ba80f3570693c
João Neto <joao72neto@gmail.com>
2025-10-11 13:37:21 -0300

    feat: create exception to validate sessions

---
+++ backend/src/main/java/com/company/flowmodoro/exceptions/InvalidSessionException.java
@@ -0,0 +1,7 @@
+package com.company.flowmodoro.exceptions;
+
+public class InvalidSessionException extends RuntimeException {
+    public InvalidSessionException(String message) {
+        super(message);
+    } 
+}
