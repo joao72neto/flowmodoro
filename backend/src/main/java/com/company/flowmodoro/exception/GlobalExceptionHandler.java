15969aa6d4b2769fda974e090417c90d95cfb98f
João Neto <joao72neto@gmail.com>
2025-10-11 14:05:14 -0300

    fix: add a specific notation to global exception handler

--- backend/src/main/java/com/company/flowmodoro/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/company/flowmodoro/exception/GlobalExceptionHandler.java
@@ -2,12 +2,14 @@ package com.company.flowmodoro.exception;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.company.flowmodoro.dto.ErrorResponse;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(InvalidSessionException.class)
     public ResponseEntity<ErrorResponse> handleInvalidSession(InvalidSessionException ex) {
         ErrorResponse errorResponse = ErrorResponse.builder()
                 .message("Invalid Session")
