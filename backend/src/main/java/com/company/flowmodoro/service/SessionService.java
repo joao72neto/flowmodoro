0f94dbc2c40de432cdb5f4b4fe25fc0326eeadb9
João Neto <joao72neto@gmail.com>
2025-10-11 13:44:49 -0300

    feat: add validations when saving a new session

--- backend/src/main/java/com/company/flowmodoro/service/SessionService.java
+++ backend/src/main/java/com/company/flowmodoro/service/SessionService.java
@@ -3,6 +3,7 @@ package com.company.flowmodoro.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.company.flowmodoro.exceptions.InvalidSessionException;
 import com.company.flowmodoro.model.entity.Session;
 import com.company.flowmodoro.repository.SessionRespository;
 
@@ -11,8 +12,24 @@ public class SessionService {
 
     @Autowired
     private SessionRespository sessionRespository;
-    
+
     public Session save(Session session) {
+        validateSessions(session);
         return sessionRespository.save(session);
+    }
+
+    private void validateSessions(Session session) {
+
+        if (session.getFocus() <= 0) {
+            throw new InvalidSessionException("Focus needs to be greater than 0");
+        }
+
+        if (session.getRest() <= 0) {
+            throw new InvalidSessionException("Rest needs to be greater than 0");
+        }
+
+        if (session.getInterruptions() < 0) {
+            throw new InvalidSessionException("Interruptions can't be less than 0");
+        }
     }
 }
