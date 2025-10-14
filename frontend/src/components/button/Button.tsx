0e4ce2036d988fb5ec7700a841f06cf83bbc1b9e
João Neto <joao72neto@gmail.com>
2025-10-14 16:32:46 -0300

    style: create timer component with tailwind

---
+++ frontend/src/components/button/Button.tsx
@@ -0,0 +1,9 @@
+function Button({ text }: { text: string }) {
+  return (
+    <button>
+      {text}
+    </button>
+  );
+}
+
+export default Button;
--- frontend/src/components/timer/Timer.tsx
+++ frontend/src/components/timer/Timer.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import './Timer.css'
+import Button from "../button/Button";
 
 function Timer() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className='timer'>
-      <p>Hello World!</p>
-      <button onClick={() => setCount(count + 1)}>Click {count}</button>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <p className="text-2xl">Flowmodoro</p>
+      <div className="flex flex-col justify-center items-center border-2 border-white/25 rounded-full p-20 m-8 w-65 h-65 cursor-pointer">
+        <p className="text-5xl text-center">00:00</p>
+      </div>
+      <Button text="Play" />
     </div>
-  )
+  );
 }
 
 export default Timer;
