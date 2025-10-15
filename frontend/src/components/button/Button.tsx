b31155ecd6dced90c4ef5c20b3bda8211cc74195
João Neto <joao72neto@gmail.com>
2025-10-15 19:26:04 -0300

    style: add style using tailwind for button

--- frontend/src/components/button/Button.tsx
+++ frontend/src/components/button/Button.tsx
@@ -3,9 +3,21 @@ type ButtonProps = {
   text?: string;
 };
 
-function Button({ icon, text}: ButtonProps) {
+function Button({ icon, text }: ButtonProps) {
   return (
-    <button>
+    <button
+      className="
+      bg-black
+      text-white 
+        pt-3 
+        pb-3 
+        w-20 
+        rounded-full 
+        cursor-pointer 
+        hover:shadow-[0_0_10px_#ffffff20] 
+        transition
+        duration-500"
+    >
       {icon && <span>{icon}</span>}
       {text && <span>{text}</span>}
     </button>
