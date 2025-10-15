97d757183b3d1f87dc15c1647f41e90fbec22531
João Neto <joao72neto@gmail.com>
2025-10-15 20:17:44 -0300

    style: style btn component

--- frontend/src/components/button/Button.tsx
+++ frontend/src/components/button/Button.tsx
@@ -1,3 +1,5 @@
+import "./Button.css"
+
 type ButtonProps = {
   icon?: React.ReactNode;
   text?: string;
@@ -5,19 +7,7 @@ type ButtonProps = {
 
 function Button({ icon, text }: ButtonProps) {
   return (
-    <button
-      className="
-      bg-black
-      text-white 
-        pt-3 
-        pb-3 
-        w-20 
-        rounded-full 
-        cursor-pointer 
-        hover:shadow-[0_0_10px_#ffffff20] 
-        transition
-        duration-500"
-    >
+    <button id="btn-flow" className="p-4 rounded-full cursor-pointer">
       {icon && <span>{icon}</span>}
       {text && <span>{text}</span>}
     </button>
