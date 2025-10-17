8da120f45b571c4b7cbd93e8a05f2ff55fedda8b
João Neto <joao72neto@gmail.com>
2025-10-16 23:07:00 -0300

    feat: set up a counter for interruptions component

--- frontend/src/components/home/Interruptions.tsx
+++ frontend/src/components/home/Interruptions.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
+
 function Interruptions() {
+  let [count, setCount] = useState(0);
+
   return (
     <div className="flex items-center justify-center gap-2 mb-6">
       <p>Interruptions</p>
-        <span className="cursor-pointer">
-          <i className="bi bi-plus-lg"/>
-        </span>
-        <span className="text-lg font-bold">0</span>
+      <span className="cursor-pointer">
+        <i onClick={() => setCount(count + 1)} className="bi bi-plus-lg" />
+      </span>
+      <span className="text-lg font-bold">{count}</span>
     </div>
   );
 }
