00d1e5f21a5ab2bfe1341132fe59964ad2a4fce0
João Neto <joao72neto@gmail.com>
2025-10-20 13:31:48 -0300

    feat: add component for button that completes tasks

---
+++ frontend/src/components/common/btn/TaskButton.tsx
@@ -0,0 +1,10 @@
+function TaskButton() {
+  return (
+    <div
+      title="Complete Task"
+      className="bg-transparent border w-5 h-5 rounded-full inline-block cursor-pointer mr-3"
+    />
+  );
+}
+
+export default TaskButton;
--- frontend/src/components/home/Tasks.tsx
+++ frontend/src/components/home/Tasks.tsx
@@ -1,4 +1,5 @@
 import IconButton from "../common/btn/IconButton";
+import TaskButton from "../common/btn/TaskButton";
 import Input from "../common/Input";
 
 function Tasks() {
@@ -10,9 +11,25 @@ function Tasks() {
       </div>
 
       <ul className="space-y-2">
-        <li className="shadow-2xl border-t border-b border-white/10 px-4 py-3 rounded flex items-center justify-between">
-          <span>Task</span>
-          <IconButton icon={<i className="bi bi-x-lg cursor-pointer" />}></IconButton>
+        <li
+          className="
+          shadow-2xl 
+          border-t 
+          border-b 
+          border-white/10 
+          px-4 
+          py-3 
+          rounded 
+          flex 
+          justify-between"
+        >
+          <div className="flex items-center">
+            <TaskButton />
+            <span>Task</span>
+          </div>
+          <IconButton
+            icon={<i className="bi bi-x-lg cursor-pointer" />}
+          ></IconButton>
         </li>
       </ul>
     </div>
