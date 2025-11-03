14b38015cdbbe1843e088c1faf3973d1af760686
João Neto <joao72neto@gmail.com>
2025-11-03 17:44:22 -0300

    feat: create hooks to get and save sessions

---
+++ frontend/src/hooks/sessions/useCreateSession.ts
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { createSessionService } from "../../api/services/sessionService";
+
+export const useCreateSession = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>();
+
+  const create = async (data: any) => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const res = await createSessionService(data);
+      return res;
+    } catch (e: any) {
+      setError("Erro ao salvar a sessão");
+      throw e;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    loading,
+    error,
+    create,
+  };
+};
---
+++ frontend/src/hooks/sessions/useSessions.ts
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import { getSessionService } from "../../api/services/sessionService";
+
+export function useSessions() {
+  const [sessions, setSessions] = useState([]);
+
+  useEffect(() => {
+    getSessionService().then(setSessions);
+  }, []);
+
+  return sessions;
+}
