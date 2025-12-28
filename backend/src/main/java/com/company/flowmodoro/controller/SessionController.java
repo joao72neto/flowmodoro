56f8a44304e3c1c53520aecca37a2f90e8b7d863
João Neto <joao72neto@gmail.com>
2025-12-28 14:01:16 -0300

    fix(backend-sessions): adjust path variable to save a new session

--- backend/src/main/java/com/company/flowmodoro/controller/SessionController.java
+++ backend/src/main/java/com/company/flowmodoro/controller/SessionController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.company.flowmodoro.dto.SessionDTO;
 import com.company.flowmodoro.mapper.SessionMapper;
@@ -28,10 +28,10 @@ public class SessionController {
     @Autowired
     private SessionMapper mapper;
 
-    @PostMapping
-    public ResponseEntity<SessionDTO> save(@RequestBody SessionDTO dto, @RequestParam("task") Long id) {
+    @PostMapping("/{id}")
+    public ResponseEntity<SessionDTO> save(@PathVariable Long id, @RequestBody SessionDTO dto) {
         Session session = sessionService.save(mapper.toEntity(dto), id);
-        return ResponseEntity.ok(mapper.toDTO(session));
+        return ResponseEntity.status(201).body(mapper.toDTO(session));
     }
 
     @GetMapping
