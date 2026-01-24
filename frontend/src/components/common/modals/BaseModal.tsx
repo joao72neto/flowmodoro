5227bcfbf11c65bb372323ebc00edd77c2ca7e45
João Neto <joao72neto@gmail.com>
2026-01-24 15:30:04 -0300

    style(modal): add min width

--- frontend/src/components/common/modals/BaseModal.tsx
+++ frontend/src/components/common/modals/BaseModal.tsx
@@ -31,7 +31,7 @@ const BaseModal = ({
     <div className="flex items-center justify-center absolute inset-0 overflow-hidden">
       <div className="bg-black absolute inset-0 opacity-50 z-50"></div>
 
-      <div className="flex flex-col border border-white/10 gap-8 z-55 backdrop-blur-2xl bg-white/10 text-white max-w-[500px] rounded-xl p-6 text-center">
+      <div className="flex flex-col border border-white/10 gap-8 z-55 backdrop-blur-2xl bg-white/10 text-white max-w-[500px] min-w-[300px] rounded-xl p-6 text-center">
         {title && <h1 className="font-bold text-xl">{title}</h1>}
 
         <div className="px-5">{children}</div>
