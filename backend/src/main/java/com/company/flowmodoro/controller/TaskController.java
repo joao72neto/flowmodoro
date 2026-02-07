a6a2a4878d4654498a685a3252b5d767dfcf7446
João Neto <joao72neto@gmail.com>
2026-02-07 13:00:22 -0300

    feat(session-controller): add endpoint to update task

--- backend/src/main/java/com/company/flowmodoro/controller/TaskController.java
+++ backend/src/main/java/com/company/flowmodoro/controller/TaskController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,9 +47,15 @@ public class TaskController {
     return ResponseEntity.status(201).body(taskMapper.toDTO(saved));
   }
 
-  @PatchMapping("/{id}")
-  public ResponseEntity<TaskDTO> update(@PathVariable Long id, @RequestBody TaskStatusDTO task) {
+  @PatchMapping("/{id}/status")
+  public ResponseEntity<TaskDTO> updateStatus(@PathVariable Long id, @RequestBody TaskStatusDTO task) {
     Task taskEntity = taskService.updateStatus(id, taskStatusMapper.toEntity(task));
+    return ResponseEntity.ok(taskMapper.toDTO(taskEntity));
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<TaskDTO> update(@PathVariable Long id, @RequestBody TaskDTO task) {
+    Task taskEntity = taskService.update(id, taskMapper.toEntity(task));
     return ResponseEntity.ok(taskMapper.toDTO(taskEntity));
   }
 
@@ -57,4 +64,5 @@ public class TaskController {
     taskService.deleteById(id);
     return ResponseEntity.noContent().build();
   }
+
 }
