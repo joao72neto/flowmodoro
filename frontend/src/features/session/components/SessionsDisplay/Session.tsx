7370b238de1f6f31b6eada17cc54e25af89206ed
João Neto <joao72neto@gmail.com>
2026-04-30 16:39:53 -0300

    fix(modal): display session modal for ungrouped sessions

--- frontend/src/features/session/components/SessionsDisplay/Session.tsx
+++ frontend/src/features/session/components/SessionsDisplay/Session.tsx
@@ -2,56 +2,38 @@ import clsx from "clsx";
 import Stack from "../../../../shared/components/Stack";
 
 import { formatToHour } from "../../../../shared/utils/number.utils";
-import SessionDetailsModal from "../SessionDetailsModal";
-import { useState } from "react";
 import type { SessionResponse } from "../../session.types";
 
 const Session = ({
   session,
   task,
+  onClick,
 }: {
   session: SessionResponse;
   task: { id: number; name: string };
+  onClick?: () => void;
 }) => {
-  const [showSessionDetailsModal, setShowSessionDetailsModal] =
-    useState<boolean>(false);
-
-  const handleDetailsModal = () => {
-    setShowSessionDetailsModal(!showSessionDetailsModal);
-  };
-
   return (
-    <>
-      <div onClick={handleDetailsModal} className="w-full">
-        <Stack
-          direction="row"
-          justify="between"
+    <div onClick={onClick} className="w-full">
+      <Stack
+        direction="row"
+        justify="between"
+        className={clsx(
+          "border border-white/5 shadow-md p-2 sm:p-3 cursor-pointer rounded-lg w-full",
+          "hover:bg-black/30 duration-200 bg-black/20",
+        )}
+      >
+        <span className="flex-1 text-sm sm:text-base">{task.name}</span>
+        <span
           className={clsx(
-            "border border-white/5 shadow-md p-2 sm:p-3 cursor-pointer rounded-lg w-full",
-            "hover:bg-white/4 duration-200 bg-black/20",
+            "text-sm bg-white/5 border border-white/5 ",
+            "px-2 py-0.5 rounded-md shadow",
           )}
         >
-          <span className="flex-1 text-sm sm:text-base opacity-70">
-            {task.name}
-          </span>
-          <span
-            className={clsx(
-              "text-xs sm:text-sm bg-white/5 border border-white/5 ",
-              "px-2 py-0.5 rounded-md shadow",
-            )}
-          >
-            {formatToHour(session.focus)}
-          </span>
-        </Stack>
-      </div>
-      {showSessionDetailsModal && (
-        <SessionDetailsModal
-          session={session}
-          task={task}
-          close={handleDetailsModal}
-        />
-      )}
-    </>
+          {formatToHour(session.focus)}
+        </span>
+      </Stack>
+    </div>
   );
 };
 
--- frontend/src/features/session/components/SessionsDisplay/TaskGroup.tsx
+++ frontend/src/features/session/components/SessionsDisplay/TaskGroup.tsx
@@ -2,71 +2,94 @@ import clsx from "clsx";
 import Stack from "../../../../shared/components/Stack";
 import { formatToHour } from "../../../../shared/utils/number.utils";
 import { capitalize } from "../../../../shared/utils/string.utils";
-import type { ITaskGroup } from "../../session.types";
+import type { ITaskGroup, SessionResponse } from "../../session.types";
 import Session from "./Session";
 
 import { AnimatedCollapse } from "../../../../shared/components/AnimatedCollapse";
 import { useState } from "react";
+import SessionDetailsModal from "../SessionDetailsModal";
 
 const TaskGroup = ({ taskGroup }: { taskGroup: ITaskGroup }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [showSessionDetailsModal, setShowSessionDetailsModal] = useState(false);
+  const [selectedSession, setSelectedSession] = useState<SessionResponse>(
+    taskGroup.sessions[0],
+  );
 
   const handleToggle = () => setIsOpen(!isOpen);
   const isTogglable = taskGroup.sessions.length > 1;
 
   return (
-    <div className="flex flex-col gap-2 w-full">
-      <Stack
-        className={clsx(
-          "w-full bg-white/4 rounded-xl p-3 sm:p-4 border border-white/10",
-          "shadow-xl transition-colors hover:bg-white/3",
-          isTogglable && "cursor-pointer",
-        )}
-        gap={3}
-        onClick={isTogglable ? handleToggle : undefined}
-      >
+    <>
+      <div className="flex flex-col gap-2 w-full">
         <Stack
-          direction="row"
-          justify="between"
-          align="center"
-          className="w-full"
+          className={clsx(
+            "w-full bg-white/4 rounded-xl p-3 sm:p-4 border border-white/10",
+            "shadow-xl transition-colors hover:bg-white/3 cursor-pointer",
+          )}
+          gap={3}
+          onClick={
+            isTogglable ? handleToggle : () => setShowSessionDetailsModal(true)
+          }
         >
-          <div className="flex gap-3 items-center">
-            {isTogglable && (
-              <span
-                className={clsx(
-                  "cursor-pointer text-lg sm:text-xl font-medium line-clamp-1",
-                  "break-all border border-white/10 rounded-lg px-3 py-1",
-                  "hover:bg-white/10 transition-colors bg-black/10",
-                )}
-                title="Expandir sessões"
-              >
-                {taskGroup.sessions.length}
-              </span>
-            )}
-
-            <span className="text-lg sm:text-xl font-medium line-clamp-1 break-all">
-              {capitalize(taskGroup.task.name)}
-            </span>
-          </div>
-          <span
-            className={clsx(
-              "text-sm sm:text-base bg-white/10 border border-white/10 ",
-              "px-3 py-1 rounded-lg shadow font-semibold",
-            )}
+          <Stack
+            direction="row"
+            justify="between"
+            align="center"
+            className="w-full"
           >
-            {formatToHour(taskGroup.taskTotalFocus)}
-          </span>
+            <div className="flex gap-3 items-center">
+              {isTogglable && (
+                <span
+                  className={clsx(
+                    "cursor-pointer text-lg sm:text-xl font-medium line-clamp-1",
+                    "break-all border border-white/10 rounded-lg px-3 py-1",
+                    "hover:bg-black/20 transition-colors bg-black/10",
+                  )}
+                  title="Expandir sessões"
+                >
+                  {taskGroup.sessions.length}
+                </span>
+              )}
+              <span className="text-lg sm:text-xl font-medium line-clamp-1 break-all">
+                {capitalize(taskGroup.task.name)}
+              </span>
+            </div>
+            <span
+              className={clsx(
+                "text-sm sm:text-base bg-white/10 border border-white/10 ",
+                "px-3 py-1 rounded-lg shadow font-semibold",
+              )}
+            >
+              {formatToHour(taskGroup.taskTotalFocus)}
+            </span>
+          </Stack>
         </Stack>
-      </Stack>
-      <AnimatedCollapse show={isOpen}>
-        <Stack gap={2} className="px-1 border-white/5 w-full">
-          {taskGroup.sessions.map((session) => (
-            <Session key={session.id} session={session} task={taskGroup.task} />
-          ))}
-        </Stack>
-      </AnimatedCollapse>
-    </div>
+        <AnimatedCollapse show={isOpen}>
+          <Stack gap={2} className="px-1 border-white/5 w-full">
+            {taskGroup.sessions.map((session) => (
+              <Session
+                key={session.id}
+                session={session}
+                task={taskGroup.task}
+                onClick={() => {
+                  setSelectedSession(session);
+                  setShowSessionDetailsModal(true);
+                }}
+              />
+            ))}
+          </Stack>
+        </AnimatedCollapse>
+      </div>
+
+      {showSessionDetailsModal && (
+        <SessionDetailsModal
+          session={selectedSession}
+          task={taskGroup.task}
+          close={() => setShowSessionDetailsModal(false)}
+        />
+      )}
+    </>
   );
 };
 
