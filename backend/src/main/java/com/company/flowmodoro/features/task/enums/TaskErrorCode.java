4c05d8ddbd09ededabe9f760b48c0e9abb1a7ec2
João Neto <joao72neto@gmail.com>
2026-06-11 14:06:47 -0300

    chore(backend): remove task feature and related dependencies

--- backend/src/main/java/com/company/flowmodoro/features/task/enums/TaskErrorCode.java
+++
@@ -1,9 +0,0 @@
-package com.company.flowmodoro.features.task.enums;
-
-import com.company.flowmodoro.shared.exception.ErrorCode;
-
-public enum TaskErrorCode implements ErrorCode {
-
-	TASK_NOT_FOUND, TASK_ID_CAN_NOT_BE_NULL, INVALID_TASK_NAME, INVALID_TASK_CHECKED_STATUS,
-
-}
--- backend/src/main/java/com/company/flowmodoro/shared/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/company/flowmodoro/shared/exception/GlobalExceptionHandler.java
@@ -9,20 +9,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.company.flowmodoro.features.sessions.exceptions.InvalidSessionException;
-import com.company.flowmodoro.features.task.exceptions.InvalidTaskException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(InvalidSessionException.class)
 	public ResponseEntity<ErrorResponse> handleInvalidSession(InvalidSessionException ex) {
-		ErrorResponse errorResponse = ErrorResponse.builder().code(ex.getCode()).errors(ex.getErrors()).build();
-
-		return ResponseEntity.badRequest().body(errorResponse);
-	}
-
-	@ExceptionHandler(InvalidTaskException.class)
-	public ResponseEntity<ErrorResponse> handleInvalidTask(InvalidTaskException ex) {
 		ErrorResponse errorResponse = ErrorResponse.builder().code(ex.getCode()).errors(ex.getErrors()).build();
 
 		return ResponseEntity.badRequest().body(errorResponse);
