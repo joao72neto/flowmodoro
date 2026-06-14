ad41600d4b6522c99e9f354986617f32fb0d8be1
João Neto <joao72neto@gmail.com>
2026-06-14 10:18:04 -0300

    feat(project): delete project with mock data

--- frontend/src/features/projects/components/Projects/Project.tsx
+++ frontend/src/features/projects/components/Projects/Project.tsx
@@ -1,11 +1,25 @@
 import DropDownMenu from "../../../../shared/components/DropDownMenu";
 import { BsThreeDotsVertical } from "react-icons/bs";
+import type { ProjectType } from "../../projects.types";
 
-const Project = ({ name = "Project" }: { name: string }) => {
+const Project = ({
+  name = "Project",
+  onDelete,
+  onEdit,
+}: {
+  name: string;
+  onDelete?: () => void;
+  onEdit?: () => void;
+}) => {
   return (
     <div className="p-4 rounded-lg bg-neutral-80 flex justify-between items-center border border-border">
       <span>{name}</span>
-      <DropDownMenu items={[{ label: "Editar" }, { label: "Excluir" }]}>
+      <DropDownMenu
+        items={[
+          { label: "Editar", onClick: onEdit },
+          { label: "Excluir", onClick: onDelete },
+        ]}
+      >
         <BsThreeDotsVertical size={20} />
       </DropDownMenu>
     </div>
--- frontend/src/features/projects/components/Projects/Projects.tsx
+++ frontend/src/features/projects/components/Projects/Projects.tsx
@@ -6,6 +6,7 @@ import { useModalFactory } from "../../../../shared/hooks/useModalFactory";
 import CreateProjectModal from "../CreateProjectModal";
 import Project from "./Project";
 
+import { GoPlus } from "react-icons/go";
 import type { CreateProjectType, ProjectType } from "../../projects.types";
 
 const Projects = () => {
@@ -22,18 +23,38 @@ const Projects = () => {
     setProjects([{ id: projects.length + 1, name: project.name }, ...projects]);
   };
 
+  const handleDeleteProject = (project: ProjectType) => {
+    setProjects(projects.filter((item) => item.id !== project.id));
+  };
+
+  const handleEditProject = (project: ProjectType) => {
+    setProjects(
+      projects.map((item) =>
+        item.id === project.id ? { ...item, name: project.name } : item,
+      ),
+    );
+  };
+
   return (
     <>
       <div className="flex flex-col gap-4 p-3 w-full h-full min-h-0">
         <div className="flex flex-col gap-4 items-center">
-          <Button className="w-full" onClick={openProjectModal}>
+          <Button
+            icon={<GoPlus size={25} />}
+            className="w-full"
+            onClick={openProjectModal}
+          >
             Novo Projeto
           </Button>
           <Input placeholder="Pesquisar projeto" className="border w-full" />
         </div>
         <div className="flex-1 flex flex-col gap-2 overflow-auto contain-content scrollbar-hidden">
           {projects.map((item) => (
-            <Project key={item.id} name={item.name} />
+            <Project
+              key={item.id}
+              name={item.name}
+              onDelete={() => handleDeleteProject(item)}
+            />
           ))}
         </div>
       </div>
