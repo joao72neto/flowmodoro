fdef45e24d2bc1a51df759129e3f36d8193d602a
João Neto <joao72neto@gmail.com>
2026-07-29 15:23:59 -0300

    feat(common): add capacitor to project

---
+++ frontend/android/app/src/main/java/com/joao/flowmodoro/MainActivity.java
@@ -0,0 +1,5 @@
+package com.joao.flowmodoro;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.joao.flowmodoro',
+  appName: 'flowmodoro',
+  webDir: 'dist'
+};
+
+export default config;
