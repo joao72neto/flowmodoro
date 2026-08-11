0da71e2046c3f0168e0158620bda9b00831c6906
João Neto <joao72neto@gmail.com>
2026-08-11 10:00:28 -0300

    fix(sync): pin SyncStatus component to the top-left corner

--- frontend/src/app/MainLayout/MainLayout.tsx
+++ frontend/src/app/MainLayout/MainLayout.tsx
@@ -27,42 +27,42 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
   }, [isSidebarOpen]);
 
   return (
-    <div className="relative min-h-screen flex overflow-x-hidden items-center">
-      <div className="flex flex-col w-full">
-        <div className="flex justify-start items-center pt-5 px-5">
-          <SyncStatus />
-        </div>
-        <MainContentContainer>{children}</MainContentContainer>
+    <div className="min-h-screen flex flex-col gap-5">
+      <div className="flex justify-start items-center pt-5 px-5">
+        <SyncStatus />
       </div>
+      <div className="relative flex flex-1 overflow-x-hidden items-center">
+        <MainContentContainer>{children}</MainContentContainer>
 
-      <div
-        className={clsx(
-          "fixed inset-0 bg-black/50 backdrop-blur-md z-20 transition-all duration-300",
-          isSidebarOpen ? "opacity-100" : "opacity-0 pointer-events-none",
+        <div
+          className={clsx(
+            "fixed inset-0 bg-black/50 backdrop-blur-md z-20 transition-all duration-300",
+            isSidebarOpen ? "opacity-100" : "opacity-0 pointer-events-none",
+          )}
+          onClick={() => setIsSidebarOpen(false)}
+        />
+
+        <SideBarContainer isOpen={isSidebarOpen}>
+          <SideBar onClick={() => setIsSidebarOpen(false)} />
+        </SideBarContainer>
+
+        {!isSidebarOpen && (
+          <div className="fixed bottom-0 right-0 p-4 z-10">
+            <ExpandableButton
+              icon={
+                <PiCaretLeftBold
+                  size={25}
+                  className="transition duration-200 hover:-translate-x-1"
+                />
+              }
+              className="rounded-full!"
+              onClick={() => setIsSidebarOpen(true)}
+            >
+              Projetos e Tags
+            </ExpandableButton>
+          </div>
         )}
-        onClick={() => setIsSidebarOpen(false)}
-      />
-
-      <SideBarContainer isOpen={isSidebarOpen}>
-        <SideBar onClick={() => setIsSidebarOpen(false)} />
-      </SideBarContainer>
-
-      {!isSidebarOpen && (
-        <div className="fixed bottom-0 right-0 p-4 z-10">
-          <ExpandableButton
-            icon={
-              <PiCaretLeftBold
-                size={25}
-                className="transition duration-200 hover:-translate-x-1"
-              />
-            }
-            className="rounded-full!"
-            onClick={() => setIsSidebarOpen(true)}
-          >
-            Projetos e Tags
-          </ExpandableButton>
-        </div>
-      )}
+      </div>
     </div>
   );
 };
