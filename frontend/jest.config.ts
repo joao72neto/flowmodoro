32949b2db8a57cc439c6a8ba3cfddfb244cbb1e7
João Neto <joao72neto@gmail.com>
2026-08-12 15:14:48 -0300

    fix(tests): solve jest conflict with uuid

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -3,6 +3,9 @@ import type { Config } from "jest";
 const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
+  moduleNameMapper: {
+    "^uuid$": "<rootDir>/test/mocks/uuid.js",
+  },
 };
 
 export default config;
