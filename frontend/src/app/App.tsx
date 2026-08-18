c23832d67a1c7223e8c9b6d8a1f28928dc84c0c2
João Neto <joao72neto@gmail.com>
2026-08-18 16:08:48 -0300

    feat(login): add page

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
