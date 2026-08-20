2b9482344c75b3105943155d213942edb6a855ed
João Neto <joao72neto@gmail.com>
2026-08-20 11:12:38 -0300

    style(login): add page structure and base design

--- frontend/src/app/pages/Login.tsx
+++ frontend/src/app/pages/Login.tsx
@@ -1,5 +1,59 @@
+import Input from "../../shared/components/inputs/Input";
+import Button from "../../shared/components/buttons/Button/Button";
+import clsx from "clsx";
+import Stack from "../../shared/components/Stack";
+import { IoLogInOutline } from "react-icons/io5";
+
 const Login = () => {
-  return <div className="p-4">Login</div>;
+  return (
+    <div className="flex min-h-0 h-screen">
+      <div
+        className={clsx(
+          "flex flex-col border-r border-border p-6 w-full gap-4",
+          "max-w-xl shadow-[8px_0_20px_rgba(0,0,0,0.30)] z-10",
+        )}
+      >
+        <div className="flex items-center gap-2">
+          <img src="/flowmodoro-icon.svg" alt="App logo" className="w-10" />
+          <p className="text-2xl">Flowmodoro</p>
+        </div>
+
+        <section className="flex flex-col justify-center gap-4 flex-1">
+          <h1 className="text-2xl mb-4">Login</h1>
+
+          <div className="flex flex-col gap-2">
+            <label>E-mail</label>
+            <Input placeholder="Ex: exemplo@email.com" />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <label>Senha</label>
+            <Input placeholder="Digite a sua senha" />
+          </div>
+
+          <div className="flex items-center gap-2 text-sm text-neutral-20">
+            <a
+              href="/"
+              className={clsx(
+                "border-r border-border pr-2",
+                "hover:text-neutral-10 hover:underline",
+              )}
+            >
+              Esqueceu a senha?
+            </a>
+            <a href="/" className="hover:text-neutral-10 hover:underline">
+              Cadastre-se
+            </a>
+          </div>
+
+          <Stack align="left" className="mt-4">
+            <Button icon={<IoLogInOutline size={21} />}>Entrar</Button>
+          </Stack>
+        </section>
+      </div>
+      <div className="flex items-center flex-1 bg-neutral-20"></div>
+    </div>
+  );
 };
 
 export default Login;
