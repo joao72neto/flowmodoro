fec9419e0fa7863f604ca5840167ee6bad854754
João Neto <joao72neto@gmail.com>
2026-08-21 11:02:07 -0300

    perf(session-modal): add ModalContainer variant without using framer motion

--- frontend/src/shared/components/Modal/ModalContainer.tsx
+++ frontend/src/shared/components/Modal/ModalContainer.tsx
@@ -10,14 +10,9 @@ const sizes = {
   lg: "max-w-[800px]",
 };
 
-const panelVariants = {
+const variants = {
   hidden: { opacity: 0, scale: 0.96 },
   visible: { opacity: 1, scale: 1 },
-};
-
-const panelVariantsLight = {
-  hidden: { scale: 0.98 },
-  visible: { scale: 1 },
 };
 
 const ModalContainer = ({
@@ -45,50 +40,74 @@ const ModalContainer = ({
 
   const modalRoot = document.body;
 
-  return createPortal(
-    <AnimatePresence>
-      {isOpen && (
-        <motion.div
-          className={clsx(
-            "fixed inset-0 flex items-center justify-center z-50 bg-black/60 p-4",
-            enableHeavyAnimations ? "backdrop-blur-lg" : "backdrop-blur-xs",
-          )}
-          onClick={() => close?.()}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{
-            duration: enableHeavyAnimations ? 0.2 : 0.15,
-            ease: "easeOut",
-          }}
-        >
+  if (enableHeavyAnimations) {
+    return createPortal(
+      <AnimatePresence>
+        {isOpen && (
           <motion.div
-            layout={enableHeavyAnimations ? "position" : false}
             className={clsx(
-              "flex flex-col gap-8 text-center",
-              "p-6 sm:p-8 rounded-3xl w-full relative max-h-[90vh] bg-neutral-100",
-              "overflow-auto overscroll-contain border border-border",
-              "shadow-xl",
-              sizes[size],
-              className,
+              "fixed inset-0 flex items-center justify-center z-50 bg-black/60 p-4",
+              "backdrop-blur-lg",
             )}
-            onClick={(e) => e.stopPropagation()}
-            initial="hidden"
-            animate="visible"
-            exit="hidden"
-            variants={
-              enableHeavyAnimations ? panelVariants : panelVariantsLight
-            }
+            onClick={() => close?.()}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
             transition={{
-              duration: enableHeavyAnimations ? 0.2 : 0.15,
+              duration: 0.2,
               ease: "easeOut",
             }}
           >
-            {children}
+            <motion.div
+              layout={"position"}
+              className={clsx(
+                "flex flex-col gap-8 text-center",
+                "p-6 sm:p-8 rounded-3xl w-full relative max-h-[90vh] bg-neutral-100",
+                "overflow-auto overscroll-contain border border-border",
+                "shadow-xl",
+                sizes[size],
+                className,
+              )}
+              onClick={(e) => e.stopPropagation()}
+              initial="hidden"
+              animate="visible"
+              exit="hidden"
+              variants={variants}
+              transition={{
+                duration: 0.2,
+                ease: "easeOut",
+              }}
+            >
+              {children}
+            </motion.div>
           </motion.div>
-        </motion.div>
-      )}
-    </AnimatePresence>,
+        )}
+      </AnimatePresence>,
+      modalRoot,
+    );
+  }
+
+  return createPortal(
+    isOpen && (
+      <div
+        className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 p-4 backdrop-blur-lg"
+        onClick={() => close?.()}
+      >
+        <div
+          className={clsx(
+            "flex flex-col gap-8 text-center",
+            "relative w-full max-h-[90vh] rounded-3xl bg-neutral-100 p-6 sm:p-8",
+            "animate-fade-in overflow-auto overscroll-contain border border-border",
+            "shadow-xl",
+            sizes[size],
+            className,
+          )}
+          onClick={(e) => e.stopPropagation()}
+        >
+          {children}
+        </div>
+      </div>
+    ),
     modalRoot,
   );
 };
